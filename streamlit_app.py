163ea20ca91f32255d984891119a96a48ba67aa0
Himanshi Gupta <himanshigupta8899@gmail.com>
2025-06-30 16:11:39 +0530

    modified

--- streamlit_app.py
+++ streamlit_app.py
@@ -507,12 +507,7 @@ def main():
         ctx = webrtc_streamer(
             key="stream",
             video_processor_factory=video_processor,
-            frontend_rtc_configuration={
-                "iceServers": [
-                    {"urls": ["stun:stun.l.google.com:19302"]}
-                ]
-            },
-            server_rtc_configuration={
+            rtc_configuration={
                 "iceServers": [
                     {"urls": ["stun:stun.l.google.com:19302"]}
                 ]
